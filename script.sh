bae37d9c16e1219508e70a7f34a421ca8ef4bf8d
yourworstnightmare_ <134671973+yourworstnightmare1@users.noreply.github.com>
2023-11-17 11:35:00 -0600

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+# Created by yourworstnightmare1/ReBlock
+# View the packageExpander git repo here -- https://github.com/yourworstnightmare1/packageexpander
+
+# Replace example directories with your own!!!
+
+pkgutil --expand /your/dir/here /extract/dir/here
+tar -xvf /payload/dir/here -C /extract/dir/here
